5740772b872e49622be80949e6e9911dcb87b4b9
Francisco Antonio <franciscoantonio53189@gmai.com>
2026-08-26 08:47:49 -0300

    testes

---
+++ src/main/java/Testes/saldodebite.java
@@ -0,0 +1,11 @@
+package Testes;
+
+import java.util.Scanner;
+
+public class saldodebite {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ola mundo" + "");
+
+    }
+}
