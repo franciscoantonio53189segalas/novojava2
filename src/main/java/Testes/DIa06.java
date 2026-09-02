0d874d34db14f8b1cac2a55feaa63631429fec70
Francisco Antonio <franciscoantonio53189@gmai.com>
2026-09-02 09:47:45 -0300

    novo exercicio

---
+++ src/main/java/Testes/DIa06.java
@@ -0,0 +1,15 @@
+package Testes;
+
+import java.util.Scanner;
+
+public class DIa06 {
+   public static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+
+
+    }
+}
---
+++ src/main/java/Testes/diA6.java
@@ -0,0 +1,16 @@
+package Testes;
+
+import java.util.Scanner;
+
+public class diA6 {
+   public static void main() {
+        Scanner scanner = new Scanner(System.in);
+       System.out.print("Digite seu número: ");
+       int numero = Integer.parseInt(scanner.nextLine());
+       numero *= 2;
+       System.out.println("o dobro do seu número é: " + numero);
+
+
+
+    }
+}
